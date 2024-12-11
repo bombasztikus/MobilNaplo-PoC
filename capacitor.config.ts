0b6f1e1529f53929983856a9a673fb4ea317a0c8
Halász Bendegúz <180638283+bombasztikus@users.noreply.github.com>
2024-12-11 11:18:30 +0100

    test

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.skiby.mobilnaplo',
+  appName: 'MobilNaplo',
+  webDir: 'dist'
+};
+
+export default config;
